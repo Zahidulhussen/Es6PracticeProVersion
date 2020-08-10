df386032732ae69f2ef5e267b3a222365d582c21
Zahidulhussen <mujahidbulbul01@gmail.com>
2020-08-11 00:57:57 +0600

    es6 start

---
+++ practice.js
@@ -0,0 +1,13 @@
+// const hubby = "Piya";
+// hubby = "Shela";
+// console.log(hubby);
+
+
+let numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+numbers = ["Sunny", "Shakib", "Kamal"];
+const nayok = {name: "Sakib Khan", phone: 4584};//const not changeable.
+console.log(nayok);
+ 
+
