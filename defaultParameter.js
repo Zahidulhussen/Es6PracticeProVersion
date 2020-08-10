4ebc9c596caa15c2610dc8cbadc1f1fb016702f2
Zahidulhussen <mujahidbulbul01@gmail.com>
2020-08-11 01:16:13 +0600

    default parameter done

---
+++ defaultParameter.js
@@ -0,0 +1,33 @@
+// 23.3 FUNCTION DEFAULT PARAMETER FOR NOT PROVIDED VALUES
+
+// code_01:First Way.
+
+// function add(num1, num2) {
+//     if(num2 == undefined){
+//         num2 = 0;
+//     }
+//     return num1 + num2;
+// }
+// const total = add(15, 17);
+// console.log(total);
+
+
+// Code_02: Second Way:
+
+// function add(num1, num2) {
+//     num2 = num2 || 0;
+//     return num1 + num2;
+// }
+// const total = add(15);
+// console.log(total);
+
+// Code_03: Third Way: ES6(Update)
+
+//After ES6 we write.
+
+function add(num1, num2 = 0) {
+    return num1 + num2;
+}
+const total = add(15);
+console.log(total);
+
